9df88ca67dc04d3f12ace396c09b9341969b1520
RamonUnch <74856804+RamonUnch@users.noreply.github.com>
2025-11-30 08:59:58 +0100

    use WinMain with optional SKIP_CRT flag

--- mkdb.bat
+++ mkdb.bat
@@ -1,7 +1,7 @@
 @del *.o
 @del zdld.exe
 
-set CFLAGS=-m32 -Og -std=c89 -pedantic -fanalyzer -gdwarf-2 -D_UNICODE -DUNICODE -nostdlib -march=i686 -lkernel32 -luser32 -lshell32 -lmsvcrt -lcomdlg32 -ladvapi32 -lshlwapi -e_MyMain -D__USE_MINGW_ANSI_STDIO=0 -Wall
+set CFLAGS=-m32 -Og -std=c89 -pedantic -fanalyzer -gdwarf-2 -D_UNICODE -DUNICODE -municode -march=i686 -lkernel32 -luser32 -lshell32 -lmsvcrt -lcomdlg32 -ladvapi32 -lshlwapi -Wall
 
 windres zdl.rc -o rez.o -Fpe-i386
 
@@ -9,7 +9,7 @@ windres zdl.rc -o rez.o -Fpe-i386
 ::gcc -c dlg.c -o dlg.o %CFLAGS%
 ::gcc -c sub.c -o sub.o %CFLAGS%
 ::
-gcc zdl.c cfg.c dlg.c sub.c rez.o -o zdl.exe %CFLAGS% -mwindows -Wl,-nxcompat -Wl,--no-seh -Wl,-dynamicbase
+gcc zdl.c cfg.c dlg.c sub.c rez.o -o zdld.exe %CFLAGS% -mwindows
 
-::@Copy /B zdl.exe D:\Jeux\PRTS\ZDL\zdla.exe
+::@Copy /B zdld.exe D:\Jeux\PRTS\ZDL\zdld.exe
 @set CFLAGS=
